f4391d4939139d71e05618e8a32ae9aab10e95ec
shroida <shroida012@gmail.com>
2025-02-18 08:33:03 +0200

    return Failure or Success

--- lib/features/posts/domain/repos/post_repo.dart
+++ lib/features/posts/domain/repos/post_repo.dart
@@ -4,7 +4,7 @@ import 'package:dartz/dartz.dart';
 
 abstract class PostRepo {
   Future<Either<Failure, List<Post>>> getAllPosts();
-  Future<Either<Failure, bool>> deletePost(int id);
-  Future<Either<Failure, bool>> updatePost(Post id);
-  Future<Either<Failure, bool>> addPost(Post id);
+  Future<Either<Failure, Unit>> deletePost(int id);
+  Future<Either<Failure, Unit>> updatePost(Post id);
+  Future<Either<Failure, Unit>> addPost(Post id);
 }
