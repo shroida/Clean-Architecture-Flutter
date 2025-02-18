da87accc287331630e691e0451df10c2ca4fcee1
shroida <shroida012@gmail.com>
2025-02-18 08:32:33 +0200

    call Failure in repo

--- lib/features/posts/domain/repos/post_repo.dart
+++ lib/features/posts/domain/repos/post_repo.dart
@@ -1,8 +1,10 @@
+import 'package:clean_architecture_flutter/core/error/failures.dart';
 import 'package:clean_architecture_flutter/features/posts/domain/entities/post.dart';
+import 'package:dartz/dartz.dart';
 
 abstract class PostRepo {
-  Future<List<Post>> getAllPosts();
-  Future<bool> deletePost(int id);
-  Future<bool> updatePost(Post id);
-  Future<bool> addPost(Post id);
+  Future<Either<Failure, List<Post>>> getAllPosts();
+  Future<Either<Failure, bool>> deletePost(int id);
+  Future<Either<Failure, bool>> updatePost(Post id);
+  Future<Either<Failure, bool>> addPost(Post id);
 }
