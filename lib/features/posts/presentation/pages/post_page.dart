50021f76e85f5d88d7ba2088d1f183727335f3c1
shroida <shroida012@gmail.com>
2025-02-19 12:15:33 +0200

    seprate post card

--- lib/features/posts/presentation/pages/post_page.dart
+++ lib/features/posts/presentation/pages/post_page.dart
@@ -1,7 +1,5 @@
-import 'package:clean_architecture_flutter/features/posts/presentation/widgets/body_post.dart';
-import 'package:clean_architecture_flutter/features/posts/presentation/widgets/id_post.dart';
-import 'package:clean_architecture_flutter/features/posts/presentation/widgets/title_post.dart';
 import 'package:clean_architecture_flutter/features/posts/presentation/widgets/app_bar_widget.dart';
+import 'package:clean_architecture_flutter/features/posts/presentation/widgets/card_post.dart';
 import 'package:flutter/material.dart';
 
 class PostPage extends StatelessWidget {
@@ -35,27 +33,7 @@ class PostPage extends StatelessWidget {
         itemCount: posts.length,
         itemBuilder: (context, index) {
           final post = posts[index];
-          return Card(
-            margin: const EdgeInsets.symmetric(vertical: 12),
-            shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-            elevation: 8,
-            color: Colors.white,
-            shadowColor: Colors.deepPurpleAccent.withOpacity(0.5),
-            child: Padding(
-              padding: const EdgeInsets.all(20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  TitlePost(post: post),
-                  const SizedBox(height: 8),
-                  BodyPost(post: post),
-                  const SizedBox(height: 15),
-                  IDPost(post: post),
-                ],
-              ),
-            ),
-          );
+          return CardPost(post: post);
         },
       ),
     );
---
+++ lib/features/posts/presentation/widgets/card_post.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:clean_architecture_flutter/features/posts/presentation/widgets/body_post.dart';
+import 'package:clean_architecture_flutter/features/posts/presentation/widgets/id_post.dart';
+import 'package:clean_architecture_flutter/features/posts/presentation/widgets/title_post.dart';
+
+class CardPost extends StatelessWidget {
+  const CardPost({
+    super.key,
+    required this.post,
+  });
+
+  final Map<String, dynamic> post;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.symmetric(vertical: 12),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+      elevation: 8,
+      color: Colors.white,
+      shadowColor: Colors.deepPurpleAccent.withOpacity(0.5),
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TitlePost(post: post),
+            const SizedBox(height: 8),
+            BodyPost(post: post),
+            const SizedBox(height: 15),
+            IDPost(post: post),
+          ],
+        ),
+      ),
+    );
+  }
+}
