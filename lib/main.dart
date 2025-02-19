4e486b8c1f9bcdc78567f30218bf7dc7b3b06a87
shroida <shroida012@gmail.com>
2025-02-19 10:11:40 +0200

    call DI in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:clean_architecture_flutter/clean_app.dart';
 import 'package:flutter/material.dart';
+import 'core/di/dependency_injection.dart' as di;
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await di.init();
   runApp(const CleanApp());
 }
