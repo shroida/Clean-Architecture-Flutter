472cda92fcafed57c86582b62713db251e811d88
shroida <shroida012@gmail.com>
2025-02-19 10:38:15 +0200

    call provider cubit in clean_app

--- lib/clean_app.dart
+++ lib/clean_app.dart
@@ -1,16 +1,22 @@
 import 'package:clean_architecture_flutter/core/theming/app_theme.dart';
+import 'package:clean_architecture_flutter/features/posts/presentation/cubit/posts_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'core/di/dependency_injection.dart' as di;
 
 class CleanApp extends StatelessWidget {
   const CleanApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: appTheme,
-      home: const Scaffold(
-        body: Center(
-          child: Text('adf'),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_)=>di.sl<PostsCubit>()),
+      ],
+      child: MaterialApp(
+        theme: appTheme,
+        home: const Scaffold(
+          body:
         ),
       ),
     );
